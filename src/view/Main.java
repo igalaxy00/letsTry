207a40d6bdbfdfcd94c94020315efb89939590fe
igalaxy00 <i.galaxy00@gmail.com>
2020-05-05 20:44:48 +0300

    нарисовал ТОЛЬ поле

--- src/view/Main.java
+++ src/view/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 import java.awt.*;
 
 public class Main extends Application {
-
+ 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
